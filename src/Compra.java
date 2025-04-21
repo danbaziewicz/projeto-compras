c562e960b390912df3aba4dd1f699dbee8ec702f
Danylo Baziewicz <danxbazi@gmail.com>
2025-04-21 07:25:44 -0300

    feat: implementar ordenação de compras por valor.

--- src/Compra.java
+++ src/Compra.java
@@ -1,4 +1,4 @@
-public class Compra {
+public class Compra implements Comparable<Compra> {
     private String descricao;
     private double valor;
     public Compra(String descricao, double valor) {
@@ -16,6 +16,11 @@ public class Compra {
 
     @Override
     public String toString() {
-        return "Compra: " + descricao + " - R$" + valor;
+        return descricao + " - R$" + valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
     }
 }
